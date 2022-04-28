41a8f0502383e29d52753708ab0a2ea4ac21cd9a
Artem Oliynyk <oliynykartemfr@gmail.com>
2022-04-28 17:05:19 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -85,40 +85,40 @@ button.addEventListener('click', function (e) {
 	current += 3;
 })
 //
-const scrollElements = document.querySelectorAll('.js-scroll');
+const scrollElements = document.querySelectorAll(".js-scroll");
 const elementInView = (el, dividend = 1) => {
-	const elementTop = el.getBoundingClientRect().top;
-	return (
-		elementTop <=
-		(window.innerHeight || document.documentElement.clientHeight) / dividend
-	);
+   const elementTop = el.getBoundingClientRect().top;
+   return (
+      elementTop <=
+      (window.innerHeight || document.documentElement.clientHeight) / dividend
+   );
 };
 const elementOutofView = (el) => {
-	const elementTop = el.getBoundingClientRect().top;
-	return (
-		elementTop > (window.innerHeight || document.documentElement.clientHeight)
-	);
+   const elementTop = el.getBoundingClientRect().top;
+   return (
+      elementTop > (window.innerHeight || document.documentElement.clientHeight)
+   );
 };
 const displayScrollElement = (element) => {
-	element.classList.add('scrolled');
+   element.classList.add("scrolled");
 };
 const hideScrollElement = (element) => {
-	element.classList.remove('scrolled');
+   element.classList.remove("scrolled");
 };
 const handleScrollAnimation = () => {
-	scrollElements.forEach((el) => {
-		if (elementInView(el, 1)) {
-			displayScrollElement(el)
-		} else if (elementOutofView(el)) {
-			hideScrollElement(el)
-		}
-	})
+   scrollElements.forEach((el) => {
+      if (elementInView(el, 1)) {
+         displayScrollElement(el)
+      } else if (elementOutofView(el)) {
+         hideScrollElement(el)
+      }
+   })
 }
 window.addEventListener("scroll", () => {
-	handleScrollAnimation(); 
+   handleScrollAnimation();
 });
 window.addEventListener("load", () => {
-	handleScrollAnimation();
+   handleScrollAnimation();
 });
 // dropdown
 if (document.querySelector('.body__explore')) {
